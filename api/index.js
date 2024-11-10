a12459781db7d6a567f0681933a90523dc5a08c2
prakash-2002 <prakashslm27@gmail.com>
2024-11-10 13:22:15 +0530

    roots added

--- api/index.js
+++ api/index.js
@@ -1,11 +1,20 @@
 import express from "express";
 import mysql from "mysql2";
-import user from "./routes/users.js"
-const app=express();
+import user from "./routes/users.js";
+import auth from "./routes/auth.js";
+import posts from "./routes/posts.js";
+import comments from "./routes/comments.js";
+import likes from "./routes/likes.js";
+const app = express();
 
-app.use("/api/users",user)
+app.use("/api/auth", auth)
+app.use("/api/users", user)
+app.use("/api/posts", posts)
+app.use("/api/comments", comments)
+app.use("/api/likes", likes)
 
 
-app.listen(8000,()=>{
+
+app.listen(8000, () => {
     console.log("server is connected to the port 8000")
 })
