80b0f2295ad6c32b2b53eb019a996be08f1e6912
prakash-2002 <prakashslm27@gmail.com>
2024-11-10 18:54:49 +0530

    connect to backend

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,7 +1,31 @@
 import { Link } from 'react-router-dom';
 import  './register.scss';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Register = () => {
+  const [inputs,setInputs]=useState({
+    username:"",
+    email:"",
+    password:"",
+    name:""
+  })
+  const [err, setErr]=useState(null);
+
+  const handleChange=(e)=>{
+
+    setInputs(prev=>({...prev,[e.target.name]:e.target.value}))
+  }
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    console.log(inputs);
+    try {
+      await axios.post(import.meta.env.VITE_API_URL+"api/auth/register",inputs)
+    } catch (error) {
+      setErr(error.response.data)
+    }
+  }
+  console.log(err)
   return (
     <div className='register'>
       <div className="card">
@@ -15,12 +39,13 @@ const Register = () => {
         </div>
         <div className="right">
             <h1>Register</h1>
-            <form>
-                <input type="text" placeholder='Username'/>
-                <input type="email" placeholder='E-mail'/>
-                <input type="password" placeholder='Password' />
-                <input type="text" placeholder='Name' />
-                <button>Register</button>
+            {err&& <div style={{color:"red"}}>{err}</div>}
+            <form onSubmit={handleSubmit}> 
+                <input onChange={handleChange} value={inputs.username} type="text" placeholder='Username'name="username"/>
+                <input onChange={handleChange} value={inputs.email} type="email" placeholder='E-mail'name="email"/>
+                <input onChange={handleChange} value={inputs.password} type="password" placeholder='Password' name="password"/>
+                <input onChange={handleChange} value={inputs.name} type="text" placeholder='Name' name="name"/>
+                <button type='submit'>Register</button>
             </form>
         </div>
       </div>
