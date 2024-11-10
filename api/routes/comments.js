040195532b2ab2fb3753609eb6d3ca6f0919fdec
prakash-2002 <prakashslm27@gmail.com>
2024-11-10 13:22:54 +0530

    all routes are added

--- api/routes/comments.js
+++ api/routes/comments.js
@@ -1,7 +1,8 @@
 import express from "express";
+import {  } from "../controllers/commentsControllers.js";
 
 const router=express.Router();
 
-
+router.get("",)
 
 export default router;
--- api/routes/likes.js
+++ api/routes/likes.js
@@ -1,7 +1,8 @@
 import express from "express";
+import {  } from "../controllers/likesControllers.js";
 
 const router=express.Router();
 
-
+router.get("",)
 
 export default router;
