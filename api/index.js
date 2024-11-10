84e3ab55c5428194730e8fe3a096a9ad854e8d0c
prakash-2002 <prakashslm27@gmail.com>
2024-11-10 18:56:25 +0530

    middlewares added

--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
 import express from "express";
-import mysql from "mysql2";
+import cors from "cors"
 import user from "./routes/users.js";
 import auth from "./routes/auth.js";
 import posts from "./routes/posts.js";
 import comments from "./routes/comments.js";
 import likes from "./routes/likes.js";
+import cookieParser from "cookie-parser";
 const app = express();
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/auth", auth)
 app.use("/api/users", user)
