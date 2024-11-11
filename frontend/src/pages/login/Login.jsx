2bf2c45fec65166da883381f5d80109d369d7c1a
prakash-2002 <prakashslm27@gmail.com>
2024-11-11 12:44:37 +0530

    connected to backend

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,13 +1,34 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import  './login.scss';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 
 const Login = () => {
+  const navigate=useNavigate();
   const {currentUser, login} = useContext(AuthContext);
 
-  const handleLogin=()=>{
-    login();
+  const [err, setErr]=useState(null);
+
+  const [inputs, setInputs]=useState({
+    username:"",
+    password:""
+  })
+
+  const handleChange=(e)=>{
+    setErr(null)
+    setInputs(prev=>({...prev,[ e.target.name]:e.target.value}))
+  }
+
+  const handleLogin=async(e)=>{
+    e.preventDefault();
+    try {
+      console.log(inputs);
+      await login(inputs);
+      navigate("/")
+    } catch (error) {
+      setErr(error.response.data)
+      console.log(error)
+    }
   }
   return (
     <div className='login'>
@@ -22,9 +43,10 @@ const Login = () => {
         </div>
         <div className="right">
             <h1>Login</h1>
+            {err && <div style={{color:"red"}}>{err}</div>}
             <form>
-                <input type="text" placeholder='Username'/>
-                <input type="password" placeholder='Password' />
+                <input type="text" placeholder='Username'onChange={handleChange}  name="username"/>
+                <input type="password" placeholder='Password' onChange={handleChange} name="password"/>
                 <button onClick={handleLogin}>Login</button>
             </form>
         </div>
