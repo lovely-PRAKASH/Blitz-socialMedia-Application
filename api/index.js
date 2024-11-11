8ecb36cb19e81310f557a4c8453f55c8b9636e25
prakash-2002 <prakashslm27@gmail.com>
2024-11-11 12:46:00 +0530

    cors added

--- api/index.js
+++ api/index.js
@@ -8,8 +8,17 @@ import likes from "./routes/likes.js";
 import cookieParser from "cookie-parser";
 const app = express();
 // middlewares
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Credentials", true)
+    next();
+})
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin:"http://localhost:5173",
+        // credentials:true,
+    }
+));
 app.use(cookieParser());
 
 app.use("/api/auth", auth)
