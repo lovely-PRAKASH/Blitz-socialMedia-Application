360015746815ed5c6c60cdd5860e9a0c9943fd4a
prakash-2002 <prakashslm27@gmail.com>
2024-11-13 22:33:46 +0530

    storage file added

--- api/index.js
+++ api/index.js
@@ -6,20 +6,35 @@ import posts from "./routes/posts.js";
 import comments from "./routes/comments.js";
 import likes from "./routes/likes.js";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 const app = express();
 // middlewares
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", true)
     next();
 })
 app.use(express.json());
 app.use(cors(
     {
-        origin:"http://localhost:5173",
-        // credentials:true,
+        origin: "http://localhost:5173",
+        credentials: true,
     }
 ));
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '../frontend/public/upload')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname)
+    },
+})
+const upload = multer({ storage: storage })
+app.use("/api/upload", upload.single("file"),(req,res)=>{
+    const file=req.file;
+    res.status(200).json(file.filename);
+})
 
 app.use("/api/auth", auth)
 app.use("/api/users", user)
@@ -32,3 +47,4 @@ app.use("/api/likes", likes)
 app.listen(8000, () => {
     console.log("server is connected to the port 8000")
 })
+
