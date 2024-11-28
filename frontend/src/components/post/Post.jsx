3485b7e1ac89985b83503744e550b7f4a5cc5557
prakash-2002 <prakashslm27@gmail.com>
2024-11-28 12:57:43 +0530

    like fn is added

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -6,53 +6,76 @@ import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
 import Comments from "../comments/Comments";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import moment from "moment";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { makeRequest } from "../../../axios";
+import { AuthContext } from "../../context/AuthContext";
 const Post = ({ post }) => {
-  const [openComments, setOpenComments]=useState(false);
-
+  const [openComments, setOpenComments] = useState(false);
+  const { currentUser } = useContext(AuthContext);
   const { isLoading, error, data } = useQuery({
     queryKey: ['likes', post.id],
-    queryFn: () => makeRequest.get("/likes?postId="+post.id).then((res) => (res.data)),
+    queryFn: () => makeRequest.get("/likes?postId=" + post.id).then((res) => (res.data)),
   });
-  console.log("likes",data);
-  
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    {
+
+     mutationFn: (liked) => {
+       if(liked) return makeRequest.delete("/likes?postId="+post.id);
+       return makeRequest.post("/likes", {postId:post.id});
+      },
+      
+        onSuccess: () => {
+          // Invalidate and refetch
+          queryClient.invalidateQueries(["likes"]);
+        },
+      
+    }
+  );
+  const handleLike=()=>{
+    mutation.mutate(data?.includes(currentUser.id))
+  }
+
   return (
     <div className='post'>
       <div className="container">
-      <div className="user">
-        <div className="userInfo">
-          <img src={post.profilePic} alt="" />
-          <div className="details">
-            <Link to={`/profile/${post.userId}`} style={{textDecoration:"none", color:"inherit"}}>
-            <span className="name">{post.name}</span>
-            </Link>
-            <span>{moment(post.createdAt).fromNow()}</span>
+        <div className="user">
+          <div className="userInfo">
+            <img src={post.profilePic} alt="" />
+            <div className="details">
+              <Link to={`/profile/${post.userId}`} style={{ textDecoration: "none", color: "inherit" }}>
+                <span className="name">{post.name}</span>
+              </Link>
+              <span>{moment(post.createdAt).fromNow()}</span>
+            </div>
+          </div>
+          <MoreHorizIcon />
+        </div>
+        <div className="content">
+          <p>{post.desc}</p>
+          <img src={"./upload/" + post.img} alt="" />
+        </div>
+        <div className="info">
+          <div className="item">
+            {isLoading?"Loading...": data?.includes(currentUser.id)
+              ? <FavoriteOutlinedIcon style={{ color: "red" }} onClick={handleLike}/>
+              : <FavoriteBorderOutlinedIcon onClick={handleLike}/>}
+            {data?.length} Likes
+          </div>
+          <div className="item" onClick={() => setOpenComments(!openComments)}>
+            <TextsmsOutlinedIcon />
+            12 Comments
+          </div>
+          <div className="item">
+            <ShareOutlinedIcon />
+            Share
           </div>
         </div>
-        <MoreHorizIcon />
-      </div>
-      <div className="content">
-        <p>{post.desc}</p>
-        <img src={"./upload/"+post.img} alt="" />
-      </div>
-      <div className="info">
-        <div className="item">
-          {/* {liked ? <FavoriteOutlinedIcon style={{color:"red"}}/> : <FavoriteBorderOutlinedIcon/>} */}
-        {data?.length} Likes
-        </div>
-        <div className="item" onClick={()=>setOpenComments(!openComments)}>
-          <TextsmsOutlinedIcon/>
-          12 Comments
-        </div>
-        <div className="item">
-          <ShareOutlinedIcon/>
-          Share
-        </div>
-      </div>
-      {openComments&&<Comments postId={post.id}/>}
+        {openComments && <Comments postId={post.id} />}
       </div>
 
     </div>
