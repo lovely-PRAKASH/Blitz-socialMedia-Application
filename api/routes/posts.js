d2533d59d425899d5f22403287b11bb56eba08dd
prakash-2002 <prakashslm27@gmail.com>
2024-12-11 11:38:40 +0530

    delete post route added

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addPost, getPosts } from "../controllers/postControllers.js";
+import { addPost, getPosts, deletePost } from "../controllers/postControllers.js";
 
 const router=express.Router();
 
-router.get("/",getPosts)
-router.post("/", addPost)
+router.get("/",getPosts);
+router.post("/", addPost);
+router.delete("/:id", deletePost)
 
 export default router;
